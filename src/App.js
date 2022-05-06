6c7e0b3e7429e85b8938d3314822766f5a1f5e4e
Adam Mackey <adambmackey@gmail.com>
2022-05-05 21:10:29 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+
+class App extends Component {
+  render() {
+    console.log(TopicBrowser)
+    return (
+    <TopicBrowser />
+    )
+  }
+}
+
+export default App;
